f0c39526145e77836bcdc63701332d5015961e53
IngridMolinaa <126828042+IngridMolinaa@users.noreply.github.com>
2023-04-25 15:54:15 -0400

    Add files via upload

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { GridComponent } from './components/grid/grid.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  {path: 'hero', component: HeroComponent},
+  {path: 'grid', component: GridComponent},
+  {path: 'dashboard', component: DashboardComponent },
+
+
+  {path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
+import { GridComponent } from './components/grid/grid.component';
+import { CadastroComponent } from './component/cadastro/cadastro.component';
+import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    LoginComponent
+    LoginComponent,
+    GridComponent,
+    CadastroComponent,
+    PageNotFoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
